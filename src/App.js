da661d356f57d787bfee1eac40dc86aae6fe79c8
Peter Brummel <pbrummel@gmail.com>
2024-03-25 20:13:00 -0500

    updated imports and other syntax

--- src/App.js
+++ src/App.js
@@ -1,47 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useReducer} from 'react';
-import { API } from 'aws-amplify';
+// V6: https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/#add-your-first-record
+import { generateClient } from 'aws-amplify/api';
 import { List, Input, Button } from 'antd';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
+import { v4 as uuid } from 'uuid';
 import { listNotes } from './graphql/queries';
 import { onCreateNote } from './graphql/subscriptions';
+import { 
+    createNote as CreateNote,
+    deleteNote as DeleteNote, 
+    updateNote as UpdateNote
+} from './graphql/mutations';
+import 'antd/dist/reset.css';
+
+const CLIENT_ID = uuid();
 
 const initialState = {
     notes: [],
     loading: true,
     error: false,
     form: { name: '', description: '' }
-}
+  }
 
 function reducer(state, action) {
     switch(action.type) {
         case 'SET_NOTES':
-          return { ...state, notes: action.notes, loading: false };
+            return { ...state, notes: action.notes, loading: false };
+        case 'ADD_NOTE':
+            return { ...state, notes: [action.note, ...state.notes]}
+        case 'RESET_FORM':
+            return { ...state, form: initialState.form }
+        case 'SET_INPUT':
+            return { ...state, form: { ...state.form, [action.name]: action.value }}    
         case 'ERROR':
-          return { ...state, loading: false, error: true };
+            return { ...state, loading: false, error: true };
         default:
-          return { ...state};
+            return { ...state};
     }
 };
 
-
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-  const client = generateClient();
+    const client = generateClient();
 
-  const fetchNotes = async() => {
-      try {
-        const notesData = await client.graphql({
-          query: listNotes
-        });
-        dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+    const fetchNotes = async() => {
+        try {
+          const notesData = await client.graphql({
+            query: listNotes
+          });
+          dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+        } catch (err) {
+          console.error(err);
+          dispatch({ type: 'ERROR' });
+        }
+      };
+
+const createNote = async () => {
+    const { form } = state // destructuring - form element out of state
+
+    if (!form.name || !form.description) {
+        return alert('please enter a name and description')
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note });
+    dispatch({ type: 'RESET_FORM' });
+
+    try {
+        await client.graphql({
+        query: CreateNote,
+        variables: { input: note }
+        })
+        console.log('successfully created note!')
+    } catch (err) {
+        console.error("error: ", err)
+    }
+};
+
+const deleteNote = async({ id }) => {
+    const index = state.notes.findIndex(n => n.id === id)
+    const notes = [
+      ...state.notes.slice(0, index), // TODO add a filter?
+      ...state.notes.slice(index + 1)];
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await client.graphql({
+        query: DeleteNote,
+        variables: { input: { id } }
+      })
+      console.log('successfully deleted note!')
       } catch (err) {
-        console.error(err);
-        dispatch({ type: 'ERROR' });
-      }
-    };
+        console.error(err)
+    }
+  };
+
+const updateNote = async(note) => {
+    const index = state.notes.findIndex(n => n.id === note.id)
+    const notes = [...state.notes]
+    notes[index].completed = !note.completed
+    dispatch({ type: 'SET_NOTES', notes})
+    try {
+        await client.graphql({
+        query: UpdateNote,
+        variables: { input: { id: note.id, completed: notes[index].completed } }
+        })
+        console.log('note successfully updated!')
+    } catch (err) {
+        console.errror(err)
+    }
+};
+    
+const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
+    };  
 
     useEffect(() => {
       fetchNotes();
@@ -60,33 +133,32 @@ const App = () => {
     }, []);
 
     const styles = {
-        container: { padding: 20 },
-        input: { marginBottom: 10 },
+        container: {padding: 20},
+        input: {marginBottom: 10},
         item: { textAlign: 'left' },
         p: { color: '#1890ff' }
     }
 
     function renderItem(item) {
-      return (
-          <List.Item 
-              style={styles.item}
-              actions={[
-                  <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>,
-                  <p style={styles.p} onClick={() => updateNote(item)}>
-                      {item.completed ? 'completed' : 'mark completed'}
-                  </p>
-                ]}>
-          <List.Item.Meta
-              title={item.name}
-              description={item.description}
-          />
-          </List.Item>
-      )
-    }; 
+        return (
+            <List.Item 
+                style={styles.item}
+                actions={[
+                    <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>,
+                    <p style={styles.p} onClick={() => updateNote(item)}>
+                        {item.completed ? 'completed' : 'mark completed'}
+                    </p>
+                  ]}>
+            <List.Item.Meta
+                title={item.name}
+                description={item.description}
+            />
+            </List.Item>
+        )
+    };  
 
-  
-  return (
-    <div style={styles.container}>
+    return (
+        <div style={styles.container}>
             <Input
                 onChange={onChange}
                 value={state.form.name}
@@ -111,7 +183,7 @@ const App = () => {
                 renderItem={renderItem}
             />
       </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
